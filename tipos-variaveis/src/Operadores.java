1e60aec990bb834ffc14282b6c430fe597798b8f
otaviogg <otavioggborges@gmail.com>
2024-04-26 18:50:05 -0300

    Operadores do java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //String nomeCompleto = "LINGUAGEM" + "JAVA";
+        //System.out.println(nomeCompleto);
+        //String concatenacao ="?"; 
+        //System.out.println(concatenacao);
+        
+       // concatenacao = 1+1+1+"1";
+        //System.out.println(concatenacao);
+
+        //concatenacao = 1+"1"+1+1;
+        //System.out.println(concatenacao);
+        int a, b;
+
+        a = 5;
+        b = 6;
+        
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+           resultado = "verdadeiro";
+        else
+           resultado = "falso";
+        */
+        
+        //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a==b) ? "verdadeiro" : "false";
+        
+        System.out.println(resultado);
+
+    }
+}
